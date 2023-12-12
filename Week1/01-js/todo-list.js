5f84e6657dc638bbb5cc9f6bbd2bbc182b56bb9d
Neetika Tripathi <64384522+nitri166@users.noreply.github.com>
2023-12-12 21:09:10 +0530

    Add files via upload

---
+++ Week1/01-js/todo-list.js
@@ -0,0 +1,49 @@
+/*
+  Implement a class `Todo` having below methods
+    - add(todo): adds todo to list of todos
+    - remove(indexOfTodo): remove todo from list of todos
+    - update(index, updatedTodo): update todo at given index
+    - getAll: returns all todos
+    - get(indexOfTodo): returns todo at given index
+    - clear: deletes all todos
+
+  Once you've implemented the logic, test your code by running
+*/
+
+class Todo {
+  constructor() {
+    this.todoList = [];
+  }
+
+  add(atodo) {
+    this.todoList.push(atodo);
+  }
+  remove(index) {
+    if (-1 < index && index < this.todoList.length) {
+      this.todoList.splice(index, 1);
+
+    }
+  }
+  update(index, atodo) {
+    if (-1 < index && index < this.todoList.length)
+
+      this.todoList[index] = atodo;
+  }
+  get(index) {
+    if (0 <= index && index < this.todoList.length) {
+      return this.todoList[index];
+    } else {
+      return null;
+    }
+  }
+
+  getAll() {
+    return this.todoList;
+  }
+  clear() {
+    return this.todoList = [];
+  }
+
+}
+
+module.exports = Todo;
